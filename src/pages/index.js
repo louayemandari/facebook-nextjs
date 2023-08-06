881239545d63501ac0bfcea44b47439bbf4e7541
unknown <mandari.louaye@gmail.com>
2023-08-06 18:00:46 +0300

    jsut added some notes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ export default function Home() {
   return (
     <div>
       main page for massnger
+      must create a page for login/sign updated
+      i will have 2 routes siging/loging in and the webapp
+        
+      
       <button
   class="btn-success ">
   Button
