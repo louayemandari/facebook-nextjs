767b36757b3413112371348931f28be6119a54cd
unknown <mandari.louaye@gmail.com>
2023-08-06 17:53:17 +0300

    i have added daisy ui and set up the supabase db schema on the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: ["cyberpunk","black","light"],
+  },
+
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
