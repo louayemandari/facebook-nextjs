79ca9886a9d818cb7f83929af91c764593b0bb76
unknown <mandari.louaye@gmail.com>
2023-08-16 23:39:27 +0300

    fixed minor bug

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { extractUsernameFromEmail } from '@/functions/extractUsernameFromEmail'
 function Users({name}) {
   return (
-    <div className='hover:bg-slate-200 rounded-md mx-auto my-1 cursor-pointer'>
+    <div className='hover:bg-slate-200 rounded-md mx-auto my-2 cursor-pointer'>
         {extractUsernameFromEmail(name)}</div>
   )
 }
